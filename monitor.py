894f5c96c057745055fe9ce2299cc3f05bd60b6f
S.S.I <70864023+manoper93@users.noreply.github.com>
2023-12-20 01:38:44 +0000

    Update monitor.py
    trying to apply rx and tx functions

--- monitor.py
+++ monitor.py
@@ -1,18 +1,57 @@
 import os
-import serial
 import time
+import ctypes
+import serial
+import wmi
 
-def sendData(cpuu, mempercused, cput, gpuperc, gpumem, gputemp):
+time.sleep(10)
+
+def rx_tx_com_ports_from_file():
     try:
-        connection = serial.Serial('COM6') # Change this to match your COM port!
+        with open('COM.txt', 'r') as file:
+            com_port_rx = file.readline().strip()  # Read the RX COM port
+            com_port_tx = file.readline().strip()  # Read the TX COM port
+            return com_port_rx, com_port_tx
+    except FileNotFoundError:
+        logging.error("File 'COM.txt' not found.")
+        return None, None
+
+def sendData(cpuu, mempercused, cput, gpuperc, gpumem, gputemp, com_port_tx):
+    try:
+        connection_tx = serial.Serial(com_port_tx)
         data = str(cpuu) + ',' + str(mempercused) + ',' + str(cput) + ',' + str(gpuperc) + ',' + str(gpumem) + ',' + str(gputemp)
-        connection.write(data.encode())
+        connection_tx.write(data.encode())
         print("Data written", data.encode())
-        connection.close()
+        connection_tx.close()
     except Exception as e:
         print(e)
 
+def recetor(com_port_rx):
+    try:
+        connection_rx = serial.Serial(com_port_rx)
+        received_data = connection_rx.read(1024)
+        print("Data received:", received_data.decode())
+        connection_rx.close()
+        return received_data.decode()
+    except Exception as e:
+        print(e)
+        return None
+
+        
+def adjust_volume(volume_change):
+    try:
+        current_volume = os.system('powershell (Get-AudioDevice -Playback -Name "Speakers").Volume')
+        new_volume = max(0, min(100, current_volume + volume_change))
+        os.system(f'powershell (Get-AudioDevice -Playback -Name "Speakers").Volume = {new_volume}')
+        logging.info(f"Volume adjusted by {volume_change}. New volume: {new_volume}")
+    except Exception as e:
+        logging.error(f"Error adjusting volume: {e}")
+
+
 while True:
+    com_port_rx, com_port_tx = rx_tx_com_ports_from_file()
+    #received_data = recetor(com_port_rx)
+    
     import wmi
     w = wmi.WMI(namespace="root\LibreHardwareMonitor")
     infoHardware = w.Sensor()
@@ -21,11 +60,9 @@ while True:
     mem_perc_used='nule'
     cpu_temp='nule'
     gpu_perc='nule'
-    
     gpu_mem='nule'
     gpu_memUsed='nule'
     gpu_memTotal='nule'
-    
     gpu_temp='nule'
 
     for sensor in infoHardware:
@@ -40,7 +77,6 @@ while True:
         if sensor.SensorType==u'Temperature' and sensor.Name==u'CPU Package':
             cpu_temp=int(sensor.Value)
             break
-
     for sensor in infoHardware:
         if sensor.SensorType==u'Load' and sensor.Name==u'GPU Core':
             gpu_perc=int(sensor.Value)
@@ -60,5 +96,10 @@ while True:
 
     gpu_mem=int((gpu_memUsed*100) / gpu_memTotal)
 
-    sendData(cpuutil, mem_perc_used, cpu_temp, gpu_perc, gpu_mem, gpu_temp)
+    #if received_data is not None:
+        #print("Received data:", received_data)
+    #else:
+        #print("Erro ao receber dados.")
+    
+    sendData(cpuutil, mem_perc_used, cpu_temp, gpu_perc, gpu_mem, gpu_temp, com_port_tx)
     time.sleep(0.5)
