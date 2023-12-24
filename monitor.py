c388b5c4d5086a57ac6767713b83ea58d6df52fd
S.S.I <70864023+manoper93@users.noreply.github.com>
2023-12-24 00:02:15 +0000

    Update monitor.py
    add 2 timers, add volume controler, add second menu

--- monitor.py
+++ monitor.py
@@ -1,105 +1,146 @@
-import os
-import time
-import ctypes
+import asyncio
 import serial
 import wmi
+import logging
+import re
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-time.sleep(10)
+logging.basicConfig(level=logging.ERROR)
 
 def rx_tx_com_ports_from_file():
     try:
         with open('COM.txt', 'r') as file:
-            com_port_rx = file.readline().strip()  # Read the RX COM port
-            com_port_tx = file.readline().strip()  # Read the TX COM port
-            return com_port_rx, com_port_tx
+            com_port = file.readline().strip()  # Read the TX COM port
+            return com_port
     except FileNotFoundError:
         logging.error("File 'COM.txt' not found.")
-        return None, None
-
-def sendData(cpuu, mempercused, cput, gpuperc, gpumem, gputemp, com_port_tx):
-    try:
-        connection_tx = serial.Serial(com_port_tx)
-        data = str(cpuu) + ',' + str(mempercused) + ',' + str(cput) + ',' + str(gpuperc) + ',' + str(gpumem) + ',' + str(gputemp)
-        connection_tx.write(data.encode())
-        print("Data written", data.encode())
-        connection_tx.close()
-    except Exception as e:
-        print(e)
-
-def recetor(com_port_rx):
-    try:
-        connection_rx = serial.Serial(com_port_rx)
-        received_data = connection_rx.read(1024)
-        print("Data received:", received_data.decode())
-        connection_rx.close()
-        return received_data.decode()
-    except Exception as e:
-        print(e)
         return None
 
-        
-def adjust_volume(volume_change):
+def change_volume(increase=True, step=0.02, zero=True):
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+    current_volume = volume.GetMasterVolumeLevelScalar()
+    if increase:
+        new_volume = min(1.0, current_volume + step)
+    else:
+        new_volume = max(0.0, current_volume - step)
+        if zero:
+            new_volume = 0
+
+    volume.SetMasterVolumeLevelScalar(new_volume, None)
+
+
+def sendData(cpuu, mempercused, cput, gpuperc, gpumem, gputemp):
     try:
-        current_volume = os.system('powershell (Get-AudioDevice -Playback -Name "Speakers").Volume')
-        new_volume = max(0, min(100, current_volume + volume_change))
-        os.system(f'powershell (Get-AudioDevice -Playback -Name "Speakers").Volume = {new_volume}')
-        logging.info(f"Volume adjusted by {volume_change}. New volume: {new_volume}")
+        com_port = rx_tx_com_ports_from_file()
+        connection_tx = serial.Serial(com_port)
+        data = f"{cpuu},{mempercused},{cput},{gpuperc},{gpumem},{gputemp}"
+        connection_tx.write(data.encode())
+        print("Tx:", data)
+        connection_tx.close()
     except Exception as e:
-        logging.error(f"Error adjusting volume: {e}")
+        logging.error(f"Error sending data: {e}")
 
+def recetor():
+    try:
+        com_port = rx_tx_com_ports_from_file()
+        connection_rx = serial.Serial(com_port, timeout=0.08)  # Adjust the timeout as needed
+        received_data = connection_rx.readline().decode('utf-8').strip()
+        connection_rx.close()
+        return received_data
+    except Exception as e:
+        logging.error(f"Error in recetor: {e}")
+        return "0"
 
-while True:
-    com_port_rx, com_port_tx = rx_tx_com_ports_from_file()
-    #received_data = recetor(com_port_rx)
-    
-    import wmi
-    w = wmi.WMI(namespace="root\LibreHardwareMonitor")
-    infoHardware = w.Sensor()
+async def get_hardware_info():
+    while True:
+        w = wmi.WMI(namespace="root\LibreHardwareMonitor")
+        infoHardware = w.Sensor()
 
-    cpuutil='nule'
-    mem_perc_used='nule'
-    cpu_temp='nule'
-    gpu_perc='nule'
-    gpu_mem='nule'
-    gpu_memUsed='nule'
-    gpu_memTotal='nule'
-    gpu_temp='nule'
+        cpuutil = 'nule'
+        mem_perc_used = 'nule'
+        cpu_temp = 'nule'
+        gpu_perc = 'nule'
+        gpu_mem = 'nule'
+        gpu_memUsed = 'nule'
+        gpu_memTotal = 'nule'
+        gpu_temp = 'nule'
 
-    for sensor in infoHardware:
-        if sensor.SensorType==u'Load' and sensor.Name==u'CPU Total':
-            cpuutil=int(sensor.Value)
-            break
-    for sensor in infoHardware:
-        if sensor.SensorType==u'Load' and sensor.Name==u'Memory':
-            mem_perc_used=int(sensor.Value)
-            break
-    for sensor in infoHardware:
-        if sensor.SensorType==u'Temperature' and sensor.Name==u'CPU Package':
-            cpu_temp=int(sensor.Value)
-            break
-    for sensor in infoHardware:
-        if sensor.SensorType==u'Load' and sensor.Name==u'GPU Core':
-            gpu_perc=int(sensor.Value)
-            break        
-    for sensor in infoHardware:
-        if sensor.Name==u'GPU Memory Used':
-            gpu_memUsed=int(sensor.Value)
-            break 
-    for sensor in infoHardware:
-        if sensor.Name==u'GPU Memory Total':
-            gpu_memTotal=int(sensor.Value)
-            break
-    for sensor in infoHardware:
-        if sensor.SensorType==u'Temperature' and sensor.Name==u'GPU Core':
-            gpu_temp=int(sensor.Value)
-            break
+        for sensor in infoHardware:
+            if sensor.SensorType == u'Load' and sensor.Name == u'CPU Total':
+                cpuutil = int(sensor.Value)
+                break
+        for sensor in infoHardware:
+            if sensor.SensorType == u'Load' and sensor.Name == u'Memory':
+                mem_perc_used = int(sensor.Value)
+                break
+        for sensor in infoHardware:
+            if sensor.SensorType == u'Temperature' and sensor.Name == u'CPU Package':
+                cpu_temp = int(sensor.Value)
+                break
+        for sensor in infoHardware:
+            if sensor.SensorType == u'Load' and sensor.Name == u'GPU Core':
+                gpu_perc = int(sensor.Value)
+                break
+        for sensor in infoHardware:
+            if sensor.Name == u'GPU Memory Used':
+                gpu_memUsed = int(sensor.Value)
+                break
+        for sensor in infoHardware:
+            if sensor.Name == u'GPU Memory Total':
+                gpu_memTotal = int(sensor.Value)
+                break
+        for sensor in infoHardware:
+            if sensor.SensorType == u'Temperature' and sensor.Name == u'GPU Core':
+                gpu_temp = int(sensor.Value)
+                break
 
-    gpu_mem=int((gpu_memUsed*100) / gpu_memTotal)
+        gpu_mem = int((gpu_memUsed * 100) / gpu_memTotal)
 
-    #if received_data is not None:
-        #print("Received data:", received_data)
-    #else:
-        #print("Erro ao receber dados.")
-    
-    sendData(cpuutil, mem_perc_used, cpu_temp, gpu_perc, gpu_mem, gpu_temp, com_port_tx)
-    time.sleep(0.5)
+        sendData(cpuutil, mem_perc_used, cpu_temp, gpu_perc, gpu_mem, gpu_temp)
+        await asyncio.sleep(0.08)  # Sleep for 0.1 seconds (100 milliseconds)
+
+async def recetor_task():
+    while True:
+        received_data = recetor()
+
+        if received_data is not None:
+            numeric_part = re.sub(r'\D', '', received_data)
+            received_data = int(numeric_part) if numeric_part.isdigit() else None
+            print("Rx:", received_data)
+        else:
+            print("Error receiving data.")
+            
+
+        if received_data == 1:
+            print(f"-------------- Aumentar volume")
+            change_volume(increase=True, step=0.1, zero=False)
+        if received_data == 2:
+            print(f"-------------- Diminuir volume")
+            change_volume(increase=False, step=0.1, zero=False)
+        if received_data == 3:
+            print(f"-------------- Volume Mute")
+            change_volume(increase=False, step=0.1, zero=True)
+        if received_data == 4:
+            print(f"-------------- Micro Ativo")
+        if received_data == 5:
+            print(f"-------------- Micro Mute")
+
+        # Process received_data as needed
+        await asyncio.sleep(0.01)  # Sleep for 1 second
+
+# Iniciar o loop assíncrono
+async def main():
+    asyncio.create_task(get_hardware_info())
+    asyncio.create_task(recetor_task())
+    while True:
+        # Adicione outras tarefas assíncronas, se necessário
+        await asyncio.sleep(0.0001)  # Sleep for 1 segundo
+
+if __name__ == "__main__":
+    asyncio.run(main())
